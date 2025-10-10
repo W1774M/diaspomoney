690609f8906384873bf8b9937cb0ffc758e31919
B177 <malarbillaudrey@gmail.com>
2025-10-10 20:09:03 +0200

    fix

--- config/database.ts
+++ config/database.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export const databaseConfig = {
   url:
     process.env["MONGODB_URI"] ||
-    "mongodb://admin:admin123@localhost:27017/diaspomoney?authSource=admin",
+    "mongodb://admin:admin123@mongodb:27017/diaspomoney?authSource=admin",
   options: {
     maxPoolSize: 10,
     serverSelectionTimeoutMS: 5000,
